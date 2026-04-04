f6f43a9319e33f30635b0815bb8b552eba062b1e
mikais13 <mikai.somerville@gmail.com>
2026-04-03 22:03:04 -1000

    feat(github): use octokit retry plugin

--- src/core/github/app.ts
+++ src/core/github/app.ts
@@ -1,4 +1,5 @@
-import { App } from "octokit";
+import { retry } from "@octokit/plugin-retry";
+import { App, Octokit } from "octokit";
 
 if (
 	!process.env.APP_ID ||
@@ -8,10 +9,13 @@ if (
 	throw new Error("Missing required environment variables");
 }
 
+const OctokitWithRetry = Octokit.plugin(retry);
+
 export const githubApp = new App({
 	appId: process.env.APP_ID,
 	privateKey: process.env.PRIVATE_KEY,
 	webhooks: {
 		secret: process.env.WEBHOOK_SECRET,
 	},
+	Octokit: OctokitWithRetry,
 });
